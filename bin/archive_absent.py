36a530c7b371b3d514c3bfeced34bbb50d139a5d
jcable <julian.cable@yahoo.com>
2021-05-25 23:21:40 +0100

    wip

--- bin/archive_absent.py
+++ bin/archive_absent.py
@@ -25,28 +25,45 @@ list = getlistid(os.environ.get('AUDIENCE'))
 with open(sys.argv[1], newline='') as csvfile:
   members = csv.DictReader(csvfile)
   gold = {}
+  gold_full = {}
   for member in members:
     id = int(member['ID'])
     gold[id] = member['Email'].strip().lower()
+    gold_full[id] = member
 
-for i in range(0, 2):
-  response = client.lists.get_list_members_info(list, fields=['members.id','members.merge_fields', 'members.email_address'], offset=1000*i, count=1000)
-  mc_members = response['members']
-  for r in mc_members:
-    gold_id = r['merge_fields']['GOLD']
-    email = r['email_address']
-    if gold_id in gold:
-      if email.lower() == gold[gold_id]:
-        pass
-      elif email == '{}@oga.org.uk'.format(gold_id):
-        if  gold[gold_id] == '':
+  
+with open('noaddr.csv', 'w', newline='') as csvfile:
+  writer = csv.DictWriter(csvfile, fieldnames=members.fieldnames)
+  writer.writeheader()
+  for i in range(0, 2):
+    response = client.lists.get_list_members_info(list, fields=['members.id','members.merge_fields', 'members.email_address'], offset=1000*i, count=1000)
+    mc_members = response['members']
+    for r in mc_members:
+      mf = r['merge_fields']
+      gold_id = mf['GOLD']
+      email = r['email_address']
+      if gold_id in gold:
+        if mf['ADDRESS'] == '':
+          print(mf['GOLD'], mf['FNAME'], mf['LNAME'], mf['MEMBER'], gold_full[gold_id]['Country'])
+          writer.writerow(gold_full[gold_id])
+        if email.lower() == gold[gold_id]:
           pass
+        elif email == f'{gold_id}@oga.org.uk'
+          if  gold[gold_id] == '':
+            pass
+          else:
+            pass
         else:
-          pass
+          print(gold_id, email, gold[gold_id])
+          hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
+          try:
+            r = client.lists.delete_list_member(list, hash)
+            print('archive', email)
+          except ApiClientError as error:
+            print('===')
+            print("Error: {}".format(error.text))
+            print(gold_id, email, gold[gold_id])
+            print('===')
+            e = json.loads(error.text)
       else:
-        print(gold_id, email, gold[gold_id])
-        hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
-        r = client.lists.delete_list_member(list, hash)
-        print('archive', email)
-    else:
-        print(gold_id, email, 'not in GOLD')
+          print(gold_id, email, 'not in GOLD')
